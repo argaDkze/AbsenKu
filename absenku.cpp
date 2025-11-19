73a15bb79a796ae81b70579e1c41249ae0fe2b77
argaDkze <muhammadargasetiawan1607@gmail.com>
2025-11-19 22:40:56 +0700

    Add constants and function declarations in absenku.cpp
    Added constants and function declarations for attendance management.

--- absenku.cpp
+++ absenku.cpp
@@ -3,4 +3,17 @@
 #include <string>
 using namespace std;
 
+const int MAX = 500;
+string nama[MAX];
+char statusArr[MAX];
+int n = 0;
 
+// Deklarasi fungsi
+void tampilkanMenu();
+void inputData();
+void bacaFile();
+void urutkanNama();
+void tampilkanAbsen();
+void rekapKehadiran();
+int cariNama(const string &key);
+void ubahStatus();
