99746cfc12281d88de14626035b32f92c6af82ef
Wenno Sivan Mahibra <wennosm@gmail.com>
2025-11-20 13:06:59 +0700

    Menampilkan hasil pencarian
    Add attendance summary output to rekapKehadiran function.

--- absenku.cpp
+++ absenku.cpp
@@ -18,6 +18,11 @@ void rekapKehadiran();
 int cariNama(const string &key);
 void ubahStatus();
 
+
+    cout << "\n===== REKAP KEHADIRAN =====\n";
+    cout << "Jumlah Hadir       : " << hadir << "\n";
+    cout << "Jumlah Tidak Hadir : " << tidak << "\n";
+}
 //================================================================//
 int cariNama(const string &key) {
     // mengasumsikan data sudah dibaca (bacaFile dipanggil oleh pemanggil)
